4fc67140676d6bd7012e86f368e3255965c2990c
Christoph Paulik <christoph.paulik@geo.tuwien.ac.at>
2016-07-21 17:49:14 +0200

    Add download and check download functions.

---
+++ datedown/down.py
@@ -0,0 +1,87 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2016,Christoph Paulik
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+Module that puts the things toghether.
+'''
+
+from multiprocessing import Pool
+import tempfile
+import os
+from functools import partial
+
+import datedown.wget as wget
+from itertools import izip as zip
+
+
+def download(urls, targets, num_proc=1, username=None, password=None):
+    """
+    Download the urls and store them at the target filenames.
+
+    Parameters
+    ----------
+    urls: iterable
+        iterable over url strings
+    targets: iterable
+        paths where to store the files
+    num_proc: int, optional
+        Number of parallel downloads to start
+    username: string, optional
+        Username to use for login
+    password: string, optional
+        Password to use for login
+    """
+    p = Pool(num_proc)
+    # partial function for Pool.map
+    cookie_file = tempfile.NamedTemporaryFile()
+    dlfunc = partial(wget.map_download,
+                     username=username,
+                     password=password,
+                     cookie_file=cookie_file.name)
+
+    p.map_async(dlfunc, zip(urls, targets)).get(9999999)
+    cookie_file.close()
+
+
+def check_downloaded(urls, targets):
+    """
+    Check if files that should be downloaded exist.
+    If not then return a list of not downloaded URLs.
+
+    Parameters
+    ----------
+    urls: iterable
+        iterable over url strings
+    targets: iterable
+        paths where to store the files
+
+    Returns
+    -------
+    not_downloaded: list
+        list of files that do not exist locally
+    """
+    not_downloaded = []
+    for url, target in zip(urls, targets):
+        if not os.path.exists(target):
+            not_downloaded.append(url)
+
+    return not_downloaded
---
+++ tests/test_down.py
@@ -0,0 +1,64 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2016,Christoph Paulik
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+'''
+Tests for downloading.
+'''
+import os
+import shutil
+from datedown.down import download
+from datedown.down import check_downloaded
+import pytest
+
+
+@pytest.fixture
+def targets(request):
+    curpath = os.path.split(os.path.abspath(__file__))[0]
+    targets = [os.path.join(curpath, "L8", "233", "081", "LC82330812015124LGN00", "LC82330812015124LGN00_MTL.txt"),
+               os.path.join(curpath, "L8", "139", "045", "LC81390452014295LGN00", "LC81390452014295LGN00_MTL.txt")]
+
+    def cleanup():
+        shutil.rmtree(os.path.join(curpath, "L8"))
+
+    try:
+        cleanup()
+    except OSError:
+        pass
+
+    request.addfinalizer(cleanup)
+
+    return targets
+
+
+def test_download(targets):
+    """
+    Test simple download of two files.
+    This library is not really made for Landsat style data but
+    this is hosted somewhat reliably by Amazon and it has a small text
+    file that can be downloaded quickly.
+    """
+    urls = ["https://s3-us-west-2.amazonaws.com/landsat-pds/L8/233/081/LC82330812015124LGN00/LC82330812015124LGN00_MTL.txt",
+            "http://landsat-pds.s3.amazonaws.com/L8/139/045/LC81390452014295LGN00/LC81390452014295LGN00_MTL.txt"]
+
+    download(urls, targets)
+    not_downloaded = check_downloaded(urls, targets)
+    assert len(not_downloaded) == 0
